2126251cfa5d71996310612ab63f3f1ab1541864
straits92 <adam.k.naas@gmail.com>
2021-08-04 17:57:22 +0200

    Update: basic allocator functionality without realloc. Traces 0-6 pass.

--- malloclab/mm.c
+++ malloclab/mm.c
@@ -10,14 +10,14 @@
  * comment that gives a high level description of your solution.
  * 
  *
- * (AN): General discussion
+ * AN: General discussion
  *
  * Provided with the lab is the memlib module which models a virtual
  * memory of MAX_HEAP ~20MB, allocated by the actual libc malloc service. 
  * The mdriver initializes this VM model by calling the mem_init() from 
- * the provided module. 
- * Within this model,
- * the student implementation (below) is initialized via mm_init. The 
+ * the provided module, and drives all the testing with its main routine.
+ *
+ * The student implementation (below) is initialized via mm_init. The 
  * core implementations of mm_malloc, mm_free, and mm_realloc are then
  * called by the mdriver on the traces provided with this lab.
  *
@@ -28,10 +28,6 @@
  * function when the heap is initialized, and subsequently whenever it
  * is extended, should mm_malloc not find a suitable block size.
  * 
- * The core student implementations use macros from mm_macros.c. 
- * mm_init() initializes the heap with CHUNKSIZE.  
- *
- *
  *
  * AN: Description of implementation
  * This implementation models the heap as an implicit free list, and 
@@ -47,7 +43,8 @@
 
 #include "mm.h"
 #include "memlib.h"
-// macros for memory management. to be included in makefile?
+
+/* macros for memory management. to be included in makefile? */
 #include "mm_macros.c"
 
 /*********************************************************
@@ -79,12 +76,22 @@ team_t team = {
 /* always points at prologue block of heap */
 static void *heap_listp; // = mem_heap_lo();
 
+/* declare helper fcns */
+static void *extend_heap (size_t words);
+static void *coalesce(void *bp);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
+static int mm_check(void);
+
+
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+
+	/* first implementation */
 	/* initial empty heap */
 	if ( (heap_listp = mem_sbrk(2*DSIZE))== (void *)-1)
 		return -1;
@@ -110,6 +117,9 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
+
+	/* basic implementation of malloc */
+	/*
     int newsize = ALIGN(size + SIZE_T_SIZE);
     void *p = mem_sbrk(newsize);
     if (p == (void *)-1)
@@ -118,13 +128,55 @@ void *mm_malloc(size_t size)
         *(size_t *)p = size;
         return (void *)((char *)p + SIZE_T_SIZE);
     }
+	*/
+
+    /* First implementation of malloc */
+
+    /* pad the requested size, return if size too small */
+    size_t asize;
+    size_t extendedsize;
+    char *bp;
+
+    if (size == 0)
+    	return NULL;
+
+    if (size <= DSIZE) {
+    	asize = 2*DSIZE;
+    } else {
+    	/* round up to nearest 8, and add 8 for overhead */
+    	asize = ((size + DSIZE + DSIZE - 1)/DSIZE) * DSIZE;
+    }
+
+    /* search for suitable block via some fit method, and allocate */
+    if ((bp = find_fit(asize)) != NULL) {
+    	place(bp, asize);
+    	return bp;
+    }
+
+
+    /* if no block large enough, extend heap, place+coalesce if possible, return bp */
+    extendedsize = MAX(asize, CHUNKSIZE);
+    if ((bp = extend_heap(extendedsize/WSIZE)) == NULL) 
+    	return NULL;
+
+    place(bp, extendedsize);
+    return bp;
+
+
 }
 
 /*
- * mm_free - Freeing a block does nothing.
+ * mm_free - Freeing a block does nothing. (in basic version)
  */
 void mm_free(void *ptr)
 {
+
+	/* First implementation of free */
+    /* mark hdr and ftr as free, and coalesce with neighbors if possible */
+    size_t size = GET_SIZE(HDRP(ptr));
+	PUT(HDRP(ptr), PACK(size, 0));
+	PUT(FTRP(ptr), PACK(size, 0));
+	coalesce(ptr);
 }
 
 /*
@@ -132,6 +184,8 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
+
+	/* basic implementation of realloc */
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
@@ -145,13 +199,14 @@ void *mm_realloc(void *ptr, size_t size)
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
+
+    /* first implementation of realloc */
+
+
 }
 
 
-/* 
- * helper functions: placement, find fits, heap checker
- *
- */
+/* helper functions */
 
 /* heap checker - where to call, what to report? 
  *
@@ -162,7 +217,7 @@ void *mm_realloc(void *ptr, size_t size)
  * do any allocated blocks overlap?
  * do the pointers in a heap block point to valid heap addresses?
  */
-int mm_check(void)
+static int mm_check(void)
 {
 
 
@@ -172,11 +227,8 @@ int mm_check(void)
 }
 
 
-/* extend the heap with a new free block. reconfigure the epilogue
- * hdr. coalesce with adjacent block if the latter is free.
- *
+/* extend the heap with a new free block.
  * called upon initializing the allocator, and whenever no block found.
- *
  */
 static void *extend_heap (size_t words)
 {
@@ -197,15 +249,87 @@ static void *extend_heap (size_t words)
 
 }
 
+/* coalesce with neighboring blocks if they are free */
 static void *coalesce(void *bp) 
 {
+	/* status of neighbor blocks, size of current */
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t size = GET_SIZE(HDRP(bp));
+
+	/* possible cases when coalescing with neighbors */
+
+	if (prev_alloc & next_alloc) { /* both allocated */
+		return bp;
+
+	} else if (prev_alloc & (!next_alloc)) { /* coalesce with next*/
+		size+= GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0)); 	/* because current hdr has new size 
+										 * as updated by previous line
+										 */
+
+	} else if ((!prev_alloc) & next_alloc) { /* coalesce with prev */
+		size+= GET_SIZE(FTRP(PREV_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+		bp = PREV_BLKP(bp);
+
+	} else { /* coalesce with both */
+		size+= GET_SIZE(FTRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+		bp = PREV_BLKP(bp);
+	}
 
 
-
-	
+	return bp;
 }
 
+/* first fit */
+static void *find_fit(size_t asize) {
 
+	void *bp = heap_listp; 
+	
+	// till we get to epilogue whose size is 0
+	while (GET_SIZE(HDRP(bp))) {
+	
+		if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp))))
+			return bp;
+		
+		bp = NEXT_BLKP(bp);
+	}
+
+	// if not found
+	return NULL;
+
+}
+
+/* placement helper */
+static void place(void *bp, size_t asize) {
+
+	size_t remainder = GET_SIZE(HDRP(bp)) - asize;
+	size_t rounded_remainder;
+
+	if ( remainder >= (2*DSIZE) ) { // split
+		
+		// shorten the allocated block
+		PUT(HDRP(bp), PACK(asize, 1));
+		PUT(FTRP(bp), PACK(asize, 1)); // ftr pos based on hdr content?
+		
+		// create the cut, free block
+		bp = NEXT_BLKP(bp);
+		rounded_remainder = DSIZE * ((remainder + DSIZE - 1)/DSIZE);
+		PUT(HDRP(bp), PACK(rounded_remainder, 0));
+		PUT(FTRP(bp), PACK(rounded_remainder, 0));
+		
+	} else { // keep current block size
+		PUT(HDRP(bp), PACK(GET_SIZE(HDRP(bp)), 1));
+		PUT(FTRP(bp), PACK(GET_SIZE(HDRP(bp)), 1)); 
+	
+	}
+	return;
+}
 
 
 
